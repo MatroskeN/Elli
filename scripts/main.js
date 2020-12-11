6b3f1e88a53eb6556459f6d5c7021594135350cf
BogdanDelukin <makaev.v1d@gmail.com>
2020-12-11 16:00:27 +0400

    fg

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    
-  });
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.right-arrow',
+    prevEl: '.left-arrow',
+  },
+});
+
+//second-slider
+
+var swiper = new Swiper('.second-slider', {
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.right-arrow2',
+    prevEl: '.left-arrow2',
+  },
+});
+
+//burger
+$("#opener").on("click", function () {
+  if (document.getElementById("opened").style.display == "none"){
+    document.getElementById("opened").style.display = "flex";
+  } else
+  document.getElementById("opened").style.display = "none";
+});
